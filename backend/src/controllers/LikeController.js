d1fa567881a3760e042cfc61ac45d349a63dc44b
Bruno Henrique <bruno.henriquy@gmail.com>
2019-08-10 21:35:53 -0300

    Match -backend -frontend

--- backend/src/controllers/LikeController.js
+++ backend/src/controllers/LikeController.js
@@ -2,8 +2,7 @@ const Dev = require('../models/Dev');
 
 module.exports = {
     async store(req, res) {
-        console.log(req.headers.user);
-        console.log(req.params.devId);        
+        console.log(req.io, req.connectedUsers);      
 
         const { user } = req.headers;
         const { devId } = req.params;        
@@ -18,7 +17,16 @@ module.exports = {
         }
 
         if(targetDev.likes.includes(loggedDev._id)){
-            console.log('Deu Match');
+            const loggedSocket = req.connectedUsers[user];
+            const targetSocket = req.connectedUsers[devId];
+
+            if (loggedSocket) {
+                req.io.to(loggedSocket).emit('match', targetDev);
+            }
+
+            if (targetSocket) {
+                req.io.to(targetSocket).emit('match', loggedDev);
+            }
         }
 
         loggedDev.likes.push(targetDev._id);
--- backend/src/server.js
+++ backend/src/server.js
@@ -4,14 +4,46 @@ const cors = require('cors');
 
 const routes = require('./routes');
 
-const server = express();
+const httpServer = express();
+
+const server = require('http').Server(httpServer);
+const io = require('socket.io')(server);
+
+// io.on('connection', socket => {
+//     console.log('Nova conexão', socket.id)
+
+//     socket.on('hello', message => {
+//         console.log(message)
+//     })
+
+//     setTimeout(() => {
+//         socket.emit('world', {
+//             message: 'OmniStack'
+//         })
+//     }, 5000);
+// });
+
+const connectedUsers = {};
+
+io.on('connection', socket => {
+    const { user } = socket.handshake.query;
+
+    connectedUsers[user] = socket.id;
+});
 
 mongoose.connect('mongodb+srv://bh:bh@cluster0-adkfc.mongodb.net/omnistack8?retryWrites=true&w=majority', {
     useNewUrlParser: true
 });
 
-server.use(cors());
-server.use(express.json());
-server.use(routes);
+httpServer.use((req, res, next) => {
+    req.io = io;
+    req.connectedUsers = connectedUsers;
+
+    return next();
+});
+
+httpServer.use(cors());
+httpServer.use(express.json());
+httpServer.use(routes);
 
 server.listen(3333);
